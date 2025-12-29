dcfefb1d4abbe1a6eb58c7433e589a57c8b6dd87
Lael <intwalilael@gmail.com>
2025-12-29 14:49:09 +0200

    Done

--- Bendik-Pharmacy-Consultancy-main/backend/routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-
-router.post('/login', adminController.login);
-router.post('/signup', adminController.signup);
-
-module.exports = router; 
